9d9691ae4193cc92eaec7aa9a3c1bb165db2d654
Anil Behera <anilbehera@MacBookAir.lan>
2024-12-09 21:32:29 +0530

    added projects

--- react-portfolio/src/components/Projects/ProjectCard.jsx
+++ react-portfolio/src/components/Projects/ProjectCard.jsx
@@ -6,14 +6,23 @@ import { getImageUrl } from "../../util";
 export const ProjectCard = ({
   project: { title, imageSrc, description, skills, live, source },
 }) => {
+
+  const handleImageClick = () => {
+    window.open(live, "_blank", "noopener,noreferrer");
+  };
   return (
     
     <div className={style.container}>
+      {/* <a  href={live} target="_blank" rel="noopener noreferrer"> */}
       <img
         src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
         className={style.image}
+        onClick={handleImageClick}
+        style={{ cursor: "pointer" }}
+        
       />
+      {/* </a> */}
       <h3 className={style.title}>{title}</h3>
       <p className={style.description}>{description}</p>
       <ul className={style.skills}>
